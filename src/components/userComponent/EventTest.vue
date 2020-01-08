f2f70f56abda22fed4653805b38be1dbed1c0231
andyou092 <andyou920@naver.com>
2020-01-08 23:25:02 +0900

    counter 글자 색 추가

--- src/components/userComponent/EventTest.vue
+++ src/components/userComponent/EventTest.vue
@@ -12,7 +12,7 @@
 <p>template</p>
 <span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">id</span>=<span class="hljs-string">"example-1"</span>&gt;</span>
 <span class="hljs-tag">&lt;<span class="hljs-name">button</span> <span class="hljs-attr">v-on:click</span>=<span class="hljs-string">"counter += 1"</span>&gt;</span>Add 1<span class="hljs-tag">&lt;/<span class="hljs-name">button</span>&gt;</span>
-<span class="hljs-tag">&lt;<span class="hljs-name">p</span>&gt;</span>위 버튼을 클릭한 횟수는 &#123;&#123; counter &#125;&#125; 번 입니다.<span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
+<span class="hljs-tag">&lt;<span class="hljs-name">p</span>&gt;</span><span style="color:red">위 버튼을 클릭한 횟수는 &#123;&#123; counter &#125;&#125; 번 입니다.</span><span class="hljs-tag">&lt;/<span class="hljs-name">p</span>&gt;</span>
 <span class="hljs-tag">&lt;/<span class="hljs-name">div</span>&gt;</span>
 </code>
       </pre>
@@ -396,7 +396,6 @@ export default {
     margin-left: 1%;
   }
   
- 
  
 
 </style>
